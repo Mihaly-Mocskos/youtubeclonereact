a510e7e294d9c18384e2685eed729120613368dd
Mocskos Mihaly <mocskos.misi@gmail.com>
2020-04-15 14:15:12 +0200

    Updates

---
+++ precache-manifest.3f8efc70909cd81798b9346c392b05ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04eafd0415fe59b3e8d37b693bfb51a7",
+    "url": "/youtubeclonereact/index.html"
+  },
+  {
+    "revision": "f16d6bd0d31582de0e42",
+    "url": "/youtubeclonereact/static/js/2.cb8787f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtubeclonereact/static/js/2.cb8787f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eeb74a75ed7d977b912",
+    "url": "/youtubeclonereact/static/js/main.2f1082b8.chunk.js"
+  },
+  {
+    "revision": "1efdf28f1be71866db83",
+    "url": "/youtubeclonereact/static/js/runtime-main.712828c6.js"
+  }
+]);
